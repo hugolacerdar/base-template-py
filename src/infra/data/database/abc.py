585bd0f6679f75cc1c92a3c3b118a904d10906a9
Hugo Lacerda <hugolacerdar@gmail.com>
2025-01-12 13:15:41 +0000

    feat: Implement abstract database class and PostgreSQL database connection pool

---
+++ src/infra/data/database/abc.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+class Database(ABC):
+    @abstractmethod
+    async def get_pool(self) -> Any:
+        """
+        Returns the database connection or pool.
+        """
+        pass
