1e2383c383a93bfeb9048d1231c26bea8583f403
Hugo Lacerda <hugolacerdar@gmail.com>
2025-01-13 22:32:08 +0000

    fix: Set default values for database configuration fields in Settings class

--- src/environment.py
+++ src/environment.py
@@ -9,11 +9,11 @@ class Settings(BaseSettings):
 	if os.environ.get('ENVIRONMENT') != 'production':
 		load_dotenv('.env.development')
 
-	db_user: str = Field(..., alias='DB_USER')
-	db_password: str = Field(..., alias='DB_PASSWORD')
-	db_host: str = Field(..., alias='DB_HOST')
-	db_port: int = Field(..., alias='DB_PORT')
-	db_name: str = Field(..., alias='DB_NAME')
+	db_user: str = Field(default=..., alias='DB_USER')
+	db_password: str = Field(default=..., alias='DB_PASSWORD')
+	db_host: str = Field(default=..., alias='DB_HOST')
+	db_port: int = Field(default=..., alias='DB_PORT')
+	db_name: str = Field(default=..., alias='DB_NAME')
 
 
 settings = Settings()
